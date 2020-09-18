34c0172e702e03aa83f3b43f23204fb3ebb5016c
Ehasanul Hoque <ehasanul@data-path.net>
2020-09-18 17:33:39 +0600

    initial commit

---
+++ Producer/Sender.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("BasicTest",false,false,false,null);
+                    var message = "Getting Started with .Net core RabbitMQ";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("", "BasicTest", null, body);
+                    Console.WriteLine("message sent : " + message);
+                }
+            }
+        }
+    }
+}
